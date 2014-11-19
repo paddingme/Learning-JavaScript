f7d8884b8f4d500e709f044531887eef7bb483c2
paddingme <huhaibo2012@gmail.com>
2014-11-20 01:45:15 +0800

    add 1-7 demo & update 1-7.md

--- Demo/test.js
+++ Demo/test.js
@@ -1,33 +1,82 @@
-
-    // 1.取得 image-gallery
-    // 2. image-gallery 的每个子女点击则响应事件
-    // 3. 要创建
-
-    // <img id="placeholder"  src="images/codercat.jpg" alt="">
-
-    // <p id="decription">请选择一张图片</p>
-
-
-
-// 1. 先创建 img src 取每个li 里的 href；
-// 2. 每个li的a 标签被点击时才触发；
-
-var images =  document.getElementById("image-gallery");
-
-var link = images.nodeChild;
-
-var img = body.insertAfter('img');
-
-
-for (var i = 0; i < link.length; i++) {
-    link[i].onclick = showImg(this);
-    img.setAttribute("src",link[i].getAttribute("href"));
-    img.setAttribute("src",link[i].getAttribute("href"));
-};
-
-function showImg(node){
-    var placeholder = document.getElementById("placeholder");
-    var decription = document.getElementById("decription");
-    var title = this.title
-
+function addLoadEvent(func) {
+    var oldonload = window.onload;
+    if (typeof oldonload != 'function') {
+        window.onload = func;
+    } else {
+        window.onload = function() {
+            oldonload();
+            func();
+        }
+    }
 }
+
+
+function insertAfter(newElement, targetElement) {
+    var parent = targetElement.parentNode;
+    if (parent.lastChild == targetElement) {
+        parent.appendChild(newElement);
+    } else {
+        parent.insertBefore(newElement, targetElement.nextSlibing);
+    }
+}
+
+
+function preparePlaceholder() {
+    if (!document.createElement) return false;
+    if (!document.createTextNode) return false;
+    if (!document.getElementById) return false;
+    if (!document.getElementById("image-gallery")) return false;
+    var placeholder = document.createElement("img");
+    placeholder.setAttribute("id", "placeholder");
+    placeholder.setAttribute("src", "images/codercat.jpg");
+    placeholder.setAttribute("alt", "my image gallery");
+
+    var description = document.createElement("p");
+    description.setAttribute("id", "description");
+    var desctext = document.createTextNode("choose an image");
+    description.appendChild(desctext);
+
+    var gallery = document.getElementById("image-gallery");
+    insertAfter(placeholder,gallery);
+    insertAfter(description,gallery);
+}
+
+function prepareGallery() {
+    if (!document.getElementsByTagName) return false;
+    if (!document.getElementById) return false;
+    if (!document.getElementById("image-gallery")) return false;
+
+    var gallery = document.getElementById("image-gallery");
+    var links = gallery.getElementsByTagName("a");
+    for (var i = 0; i < links.length; i++) {
+        links[i].onclick = function() {
+            return showPic(this);
+        }
+        links[i].onkeypress = links[i].onclick;
+    }
+}
+
+
+function showPic(whichpic) {
+    if (!document.getElementById("placeholder")) return false;
+    var source = whichpic.getAttribute("href");
+    placeholder.setAttribute("src", source);
+
+    if (!document.getElementById("description")) return false;
+    if (whichpic.getAttribute("title")) {
+        var text = whichpic.getAttribute("title");
+    } else {
+        var text = "";
+    }
+
+    var description = document.getElementById("description");
+    if (description.firstChild.nodeType == 3) {
+        description.firstChild.nodeValue = text;
+    }
+
+    return false;
+}
+
+
+addLoadEvent(preparePlaceholder);
+addLoadEvent(prepareGallery);
--- README.md
+++ README.md
@@ -13,7 +13,7 @@
 |[第四章 JavaScript 图片库](https://github.com/paddingme/Learning-JavaScript/blob/master/Book/1-4.md)|2014年10月31日|2014年11月19日|  review+2  |
 |[第五章 最佳实践](https://github.com/paddingme/Learning-JavaScript/blob/master/Book/1-5.md)|2014年11月3日|2014年11月9日|  review+2  |
 |[第六章 图片库改进版](https://github.com/paddingme/Learning-JavaScript/blob/master/Book/1-6.md)|2014年11月3日|2014年11月13日|  review+4 |
-|[第七章 动态创建标记](https://github.com/paddingme/Learning-JavaScript/blob/master/Book/1-7.md)|2014年11月14日|2014年11月19日|  review+2 |
+|[第七章 动态创建标记](https://github.com/paddingme/Learning-JavaScript/blob/master/Book/1-7.md)|2014年11月14日|2014年11月20日|  review+3 |
 
 
 ## 2.《JavaScript 高级程序设计》
